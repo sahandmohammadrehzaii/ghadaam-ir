06c77a976c04ea0e0ff4894e36798c7c751db5e5
sahandmohammadrezaii <139042771+sahandmohammadrehzaii@users.noreply.github.com>
2023-12-04 17:20:06 +0330

    hello poeple
    hello people the this version is new version of the my main and best bot ai

--- ghadaam-ir/main.py
+++ ghadaam-ir/main.py
@@ -1,113 +1,61 @@
-#in the name of god
+import random
+import tkinter as tk
+from tkinter import messagebox
 
-#coming soon
+class ChatBot:
+    def __init__(self, responses):
+        self.responses = responses
 
-# desgined by sahandmohammadrezaii and ghadaam team
+    def get_response(self, user_input):
+        user_input = user_input.lower()
+        best_match = ''
+        highest_prob = 0
 
-import re
-import long_responses as long
+        for key in self.responses:
+            prob = self.calculate_similarity(user_input, key)
+            if prob > highest_prob:
+                highest_prob = prob
+                best_match = key
 
-for i in range(1):
-   print("              به هوش مصنوعی ساخته شده توسط تیم ویژه قادم خوش آمدید")
+        return self.responses[best_match] if highest_prob >= 0.5 else "Sorry, I don't understand."
 
-#cli-1
+    def calculate_similarity(self, input_text, response_key):
+        input_words = input_text.split()
+        response_words = response_key.split()
+        common_words = set(input_words) & set(response_words)
+        similarity = len(common_words) / len(response_words)
+        return similarity
 
-for i in range(1):
-   print("")
-   
-#cli-2
-   
-for i in range(1):
-    print("")
+def send_message():
+    user_input = input_entry.get()
+    response = bot.get_response(user_input)
+    messagebox.showinfo('Bot', response)
+    input_entry.delete(0, tk.END)
 
-#cli-3
+# Define the responses for the bot
+responses = {
+    'hello': 'Hello, how can I assist you?',
+    'how are you': 'I am doing well, thank you!',
+    'weather': 'The weather is sunny today.',
+    'goodbye': 'Goodbye! Have a nice day.',
+    'default': 'Sorry, I dont have an appropriate response.'
+}
 
-for i in range(1):
-    print("")
-    
-#wellcome-massage
+# Create an instance of the chat bot
+bot = ChatBot(responses)
 
-for i in range(1):
-    print("ghadaam-ir : سلام من قادم آی آر هستم من آماده خدمت به شما در هر لحظه ای هستم")
+# Create the main window
+window = tk.Tk()
+window.title('Chat Bot')
+window.geometry('400x300')
 
-#cli-4
+# Create the input entry
+input_entry = tk.Entry(window, width=40)
+input_entry.pack(pady=20)
 
-for i in range(1):
-    print("");
-    
-#cli-5
+# Create the send button
+send_button = tk.Button(window, text='Send', command=send_message)
+send_button.pack()
 
-for i in range(1):
-    print("");
-    
-#cli-6
-
-for i in range(1):
-    print("");
-
-#massage-components
-
-def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
-    message_certainty = 0
-    has_required_words = True
-
-#massage-if & else
-
-    for word in user_message:
-        if word in recognised_words:
-            message_certainty += 1
-
-
-    percentage = float(message_certainty) / float(len(recognised_words))
-
-
-    for word in required_words:
-        if word not in user_message:
-            has_required_words = False
-            break
-
-
-    if has_required_words or single_response:
-        return int(percentage * 100)
-    else:
-        return 0
-
-
-def check_all_messages(message):
-    highest_prob_list = {}
-
-    def response(bot_response, list_of_words, single_response=False, required_words=[]):
-        nonlocal highest_prob_list
-        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
-
-#write-the-massage-ghadaam-ir    => black.cli   <+>   console.log("hello world I am ghadaam-ir")
-
-    response('سلام', ['hello', 'سلام', 'hi', 'salam',], single_response=True)
-    response('به اميد ديدار', ['به اميد ديدار', 'bye', 'goodbye', 'خداحافظ', 'باي',], single_response=True)
-    response('من هيچ گونه احساس ندارم و نمي توانم مانند انسان ها حس داشته باشم', ['حالت چطوره', 'خوبي', 'how are you', 'حالت خوبه '], required_words=['چطوري'])
-    response('خوش آمدید به قادم آی آر', ['thank', 'thanks'], single_response=True)
-    response('متشکرم از شما', ['ممنون', 'دستت درد نکنه', 'با تشکر از شما',], required_words=['code', 'palace'])
-
-
-    response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
-    response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
-
-    best_match = max(highest_prob_list, key=highest_prob_list.get)
-
-
-    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
-
-
-
-def get_response(user_input):
-    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
-    response = check_all_messages(split_message)
-    return response
-
-
-
-while True:
-    print('ghadaam-ir : ' + get_response(input('شما : ')))
-    
-while False:
-    print('ghadaam-ir = > def datebassment.getbyelement : ' + get_response(input('user = > api-cli: ')))
+# Start the main event loop
+window.mainloop()
